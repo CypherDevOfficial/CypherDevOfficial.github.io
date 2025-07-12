2c81223c9a9805972fd46cd8a3b12ad503502cae
CypherDev <1039398@lcps.org>
2025-07-12 19:40:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
 
-
-window.addEventListener('DOMContentLoaded', () => {
-  VANTA.DOTS({
-    el: "#vanta-bg",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0x264312,
-    color2: 0x36613b,
-    backgroundColor: 0x000000,
-    size: 2.10,
-    spacing: 24.00
-  });
-});
